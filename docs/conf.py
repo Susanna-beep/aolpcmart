eca2cf3a343b02b2aeaddf6b2ad3fd1571684d5c
goldaoldesktop <graceandreacchi@hotmail.com>
2026-04-30 10:52:29 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'AOL PC Mart'
+project = 'My Project'
 copyright = '2025'
-author = 'Carol Anshaw'
+author = 'Huntley'
 
 release = '1.0'
 
